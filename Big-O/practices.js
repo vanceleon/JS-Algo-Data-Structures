13275e3b4ed3ac972ec23430d92f65a87dc11115
vanceleon <vanceleon44@gmail.com>
2018-10-19 18:16:54 -0400

    performance

--- Big-O/practices.js
+++ Big-O/practices.js
@@ -1,1 +1,22 @@
-console.log("practicing my JS in CS")
+// console.log("practicing my JS in CS")
+
+function addUpTo(n) {
+    let total = 0;
+    for(let i = 0; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+// console.log(addUpTo(10000))
+
+// this function is more efficient
+function addUpTo(n) {
+    return n * (n + 1) / 2;
+}
+
+
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1)/1000} seconds.`)
