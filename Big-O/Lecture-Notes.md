3a40b00327004d813b822362efe784b586a2c739
vanceleon <vanceleon44@gmail.com>
2018-10-20 09:20:03 -0400

    charCount function

---
+++ Big-O/Lecture-Notes.md
@@ -0,0 +1,19 @@
+Understanding the problem
+    - what are the inputs that are required in the problem
+    - what are the outputs
+    - what are the edge cases for user inputs 
+    - Do you have enough information to solve the problem
+    - how should the important pieces of data be label 
+
+
+finding examples of the problem
+    and comparing it to the current problem
+
+
+Break down the problem
+    psuedo code or just comments that examplain each step of the problem
+
+
+Solve/Simplify the problem
+
+
--- Big-O/practices.js
+++ Big-O/practices.js
@@ -16,7 +16,33 @@ function addUpTo(n) {
 
 
 
-let t1 = performance.now();
-addUpTo(1000000000);
-let t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1)/1000} seconds.`)
+// let t1 = performance.now();
+// addUpTo(1000000000);
+// let t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1)/1000} seconds.`)
+
+
+
+
+
+
+function charCount(str) {
+    // make object to return at end
+    let result = {};
+    // loop over string, for each character..
+    for (let i = 0; i < str.length; i++){
+        let char = str[i].toLowerCase()
+        if (result[char]> 0){
+    // if the char is a number/letter AND key in object, add one to count
+            result[char]++;
+        }else{
+            // if the char is a number/letter AND not in object, add it to object  and set value to 1
+            result[char] = 1
+        };
+    }
+    // return object at end
+    return result
+        // if character is something else (space, period, etc.) don't do anything
+
+} 
+console.log(charCount("Renee"))
