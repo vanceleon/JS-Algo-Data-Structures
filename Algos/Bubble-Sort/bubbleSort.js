fff5860072ce054cdd171f41802fbb04d0407d57
vanceleon <vanceleon44@gmail.com>
2018-12-20 11:18:45 -0500

    working bubble sort function

--- Algos/Bubble-Sort/bubbleSort.js
+++ Algos/Bubble-Sort/bubbleSort.js
@@ -1,8 +1,12 @@
 const bubbleSort = (arr) => {
-    for(let i = 0; i < arr.length; i++){
-        for(let j = i - 1; arr.length; j++) {
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i - 1; j++) {
+            console.log("arr", arr);
             if(arr[j] > arr[j + 1]) {
+                // swap
+                let temp = arr[j];
                 arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
