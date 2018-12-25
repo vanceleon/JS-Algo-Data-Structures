c69dcfa2ca0096469ab10873e235c8cdd416354d
vanceleon <vanceleon44@gmail.com>
2018-12-25 10:06:34 -0500

    selection sort implementation

--- Algos/Selection-Sort/selectionSort.js
+++ Algos/Selection-Sort/selectionSort.js
@@ -1,11 +1,21 @@
-const selectionSort = (arr) => {
-    let min;
-    for(let i = 0; i < arr.length; i++ ){
-        let min = i;
-        for(let j = i + 1; )
-       }
-
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      console.log(i, j);
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
     }
-    return arr;
-}
-selectionSort([23,4,213,54,5])
+    if (i !== min) {
+      console.log("Before swap", arr);
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+      console.log("after swap", arr);
+    }
+  }
+  console.log(arr);
+  return arr;
+};
+selectionSort([23, 4, 213, 54, 5]);
