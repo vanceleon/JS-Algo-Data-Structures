6961b48a5dad1caf1ca1191202d26ce5a1d4cf23
vanceleon <vanceleon44@gmail.com>
2018-12-31 13:52:01 -0500

    insert function complete

--- DataStructures/BinarySearchTree/binaryTree.js
+++ DataStructures/BinarySearchTree/binaryTree.js
@@ -14,10 +14,30 @@ class BinarySearchTree {
     }
     insert(value){
         let newNode = new Node(value);
-        if (!this.root){
+        if (this.root === null){
             this.root = newNode;
             return this;
-        }  
+        }  else{
+            let current = this.root;
+            while (true ){
+                if(value === current.value) return undefined; //this is a dup check, could also figure out a count for dups
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    }else{
+                        current = current.left;
+                    }
+                }else if(value > current.value){
+                    if (current.right === null){
+                        current.right = newNode;
+                        return this;
+                    }else{
+                        current = current.right;
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -28,8 +48,11 @@ class BinarySearchTree {
 let tree = new BinarySearchTree();
 tree.root = new Node(10);
 tree.root.right = new Node(15);
-tree.root.left = new Node(7);
+tree.root.left = new Node(8);
 tree.root.left = new Node(7);
 tree.root.left.right = new Node(9);
 
+
+tree.insert(5);
+tree.insert(16);
 console.log(tree);
