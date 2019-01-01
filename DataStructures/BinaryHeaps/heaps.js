af3fcb001e0b7dd4cf5300aaa8198f72065661f8
vanceleon <vanceleon44@gmail.com>
2019-01-01 12:35:39 -0500

    extracting the max

--- DataStructures/BinaryHeaps/heaps.js
+++ DataStructures/BinaryHeaps/heaps.js
@@ -1,60 +1,67 @@
 class MaxBinaryHeap {
-    constructor() {
-        this.values = [41, 39, 33, 18, 27, 12];
-    }
-    insert(element) {
-        this.values.push(element);
-        this.bubbleUp();
-    }
-    bubbleUp(){
-        let idx = this.values.length - 1;
-        const element = this.values[idx];
-        while(idx > 0){
-            let parentIdx = Math.floor((idx - 1)/2);
-            let parent = this.values[parentIdx];
-            if (element <= parent) break;
+  constructor() {
+    this.values = [41, 39, 33, 18, 27, 12];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
 
-            this.values[parentIdx] = element;
-            this.values[idx] = parent; 
-            idx = parentIdx;
-            
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown(0);
+    }
+
+    return max;
+  }
+
+  sinkDown(idx) {
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * idx + 1;
+      let rigthChildIndex = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
         }
-    }
-
-    extractMax() {
-        const max = this.values[0];
-        const end = this.values.pop();
-        this.values[0] = end;
-        // trickle down
-        return max;
-
-
-    }
-
-    sinkDown(index) {
-        let idx = 0;
-        const length = this.values.length;
-        const element = this.values[0];
-        while(true) {
-            let leftChildIndex = 2 * idx + 1;
-            let rigthChildIndex = 2 * idx + 2;
-            let leftChild, rightChild;
-            let swap = null;
-            
-            if(leftChildIndex < length) {
-                leftChild = this.values[leftChildIndex];
-                if(leftChild > element) {
-                    swap = leftChildIndex;
-                }
-            }
-            if(rigthChildIndex < length) {
-                rightChild = this.values[rigthChildIndex];
-            }
-            if (swap === null) break;
+      }
+      if (rigthChildIndex < length) {
+        rightChild = this.values[rigthChildIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rigthChildIndex;
         }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
     }
+  }
 }
-
 
 let heap = new MaxBinaryHeap();
 heap.insert(55);
