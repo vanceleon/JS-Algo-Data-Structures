17eebe82d7433375f6e893c57b537beac55e1b58
vanceleon <vanceleon44@gmail.com>
2019-01-07 21:46:42 -0500

    radix sort implementation

--- Algos/RadixSort/lecture.md
+++ Algos/RadixSort/lecture.md
@@ -7,3 +7,16 @@
 #Radix Sort Helpers
 - In order to implement radix sort, helper functions are helpful:
 - getDigit(num, place) - returns the digit in num at the given place value
+- how many times to run helper
+    -  **digitCount(num)** - returns the number of digits in num. 
+- **mostDigits(nums)** - given an array of numbers, returns the numbers of digits in the largest numbers in the list.
+
+#Radix Sort Pseudocode 
+- define a function that accepts list of numbers
+- figure out how many digits the largest number has 
+- loop from *k = 0* up to this largest number of digits 
+- for each iteration of loop:
+    - create buckets for each digit( 0 to 9)
+    - place each number in the corresponding bucket based on its *kth* digit
+- Replace our existing array with values in our buckets, starting with 0 and going up to 9
+- return list at the end
--- Algos/RadixSort/radixSort.js
+++ Algos/RadixSort/radixSort.js
@@ -2,3 +2,33 @@
 const getDigit = (num, i) => {
     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
+
+
+const digitCount = (num) => {
+    if(num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+const mostDigits = (nums) => {
+    let maxDigits = 0;
+    for(let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits;
+}
+
+const radixSort = (nums) => {
+    let maxDigitCount = mostDigits(nums);
+    // console.log(maxDigitCount);
+    for(let k = 0; k < maxDigitCount; k++){
+        let digitBuckets = Array.from({length: 10}, () => []);
+        for (let i = 0; i < nums.length; i++){
+            let digit = getDigit(nums[i], k);
+            digitBuckets[digit].push(nums[i]);
+        }
+        nums = [].concat(...digitBuckets);
+    }
+    return nums;
+}
+
+radixSort([23,345, 5467, 12, 2345,9852]);
